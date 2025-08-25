72958f6d283f1f205d3342961351b8247ecc2e7b
JustSadSock <136240492+JustSadSock@users.noreply.github.com>
2025-08-25 14:11:23 +0300

    refactor: slot-machine reel layout

--- test/test.js
+++ test/test.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 const html = fs.readFileSync('index.html', 'utf8');
 
-if(!/@media \(orientation:portrait\)[^}]*\{[^}]*\.grid\{[^}]*grid-template-columns:1fr 1fr;/.test(html)){
-  throw new Error('Portrait layout must have two grid columns');
+if(!/class="reel"/.test(html)){
+  throw new Error('Reel container missing');
 }
 
-if(!/\.screen::before/.test(html)){
-  throw new Error('Missing screen glare effect');
+if(!/function updateReel\(\)/.test(html)){
+  throw new Error('Missing updateReel logic');
 }
 
 console.log('All tests passed');
