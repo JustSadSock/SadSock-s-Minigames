bee80a724864cad5e5c63467f7ee2883da101356
JustSadSock <136240492+JustSadSock@users.noreply.github.com>
2025-08-26 10:03:58 +0300

    Add cylindrical reel with perspective

---
+++ test/test.js
@@ -0,0 +1,88 @@
+const fs = require('fs');
+const html = fs.readFileSync('index.html', 'utf8');
+const audio = fs.readFileSync('audio.js', 'utf8');
+
+if(!/class="reel"/.test(html)){
+  throw new Error('Reel container missing');
+}
+
+if(!/function render\(\)/.test(html)){
+  throw new Error('Missing render logic');
+}
+if(!/function snap\(dir\)/.test(html)){
+  throw new Error('Missing snap function');
+}
+
+if(/class="panel"/.test(html)){
+  throw new Error('Panel should be removed');
+}
+
+if(/class="dpad"/.test(html)){
+  throw new Error('D-pad should be removed');
+}
+
+if(/id="scrollUp"/.test(html) || /id="scrollDown"/.test(html)){
+  throw new Error('Scroll arrows should be removed');
+}
+
+if(!/class="static"/.test(html)){
+  throw new Error('Static overlay missing');
+}
+
+if(/crtFlicker/.test(html)){
+  throw new Error('CRT flicker should be removed');
+}
+
+if(!/calc\(-50% \+ var\(--slotY\)\)/.test(html)){
+  throw new Error('Tiles not centered');
+}
+
+if(!/var\(--slotRot\)/.test(html)){
+  throw new Error('Rotation variable missing');
+}
+
+if(!/const cols = 2/.test(html)){
+  throw new Error('Reel should use two columns');
+}
+
+if(!/--slotBlur/.test(html)){
+  throw new Error('Blur variable missing');
+}
+
+if(!/mask-image:linear-gradient\(to bottom, transparent 0%, black 15%, black 85%, transparent 100%\)/.test(html)){
+  throw new Error('Reel mask missing');
+}
+if(!/perspective:var\(--persp\)/.test(html)){
+  throw new Error('Perspective variable missing');
+}
+
+if(!/id="settingsBtn"/.test(html)){
+  throw new Error('Settings button missing');
+}
+
+if(!/id="settingsMenu"/.test(html)){
+  throw new Error('Settings menu missing');
+}
+
+if(!/id="volume"/.test(html)){
+  throw new Error('Volume slider missing');
+}
+
+if(!/id="fullscreenBtn"/.test(html)){
+  throw new Error('Fullscreen button missing');
+}
+
+if(!/grid-template-columns:80px 24px 1fr 24px/.test(html)){
+  throw new Error('Avatar arrow alignment grid missing');
+}
+
+if(!/setVolume\(v\)/.test(audio)){
+  throw new Error('setVolume not implemented');
+}
+
+const tileCount = (html.match(/class="tile"/g)||[]).length;
+if(tileCount !== 16){
+  throw new Error('Expected 16 tiles');
+}
+
+console.log('All tests passed');
